c21c2b1826dca189c78c9d396f67fe46350e257c
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-03-13 13:32:30 +0530

    Added: Welcome to Hotel Reservation System

---
+++ src/com/hotelreservationsystem/Hotel.java
@@ -0,0 +1,7 @@
+package com.hotelreservationsystem;
+
+public class Hotel {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation System");
+    }
+}
